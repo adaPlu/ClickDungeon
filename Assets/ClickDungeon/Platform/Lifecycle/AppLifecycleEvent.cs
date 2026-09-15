071c4390fc9cbd3391058ce128034fcb3bc12b2e
Adam Pluguez <70963681+adaPlu@users.noreply.github.com>
2026-09-15 11:13:11 -0700

    feat: add mobile lifecycle event vocabulary

---
+++ Assets/ClickDungeon/Platform/Lifecycle/AppLifecycleEvent.cs
@@ -0,0 +1,10 @@
+namespace ClickDungeon.Platform.Lifecycle
+{
+    public enum AppLifecycleEvent
+    {
+        Paused = 0,
+        Backgrounded = 1,
+        Resumed = 2,
+        SystemBack = 3
+    }
+}
