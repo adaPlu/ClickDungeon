88a417f15b9b4928654ae9351c45e7a56dbbf450
Adam Pluguez <70963681+adaPlu@users.noreply.github.com>
2026-09-16 22:32:35 -0700

    feat: advance run save schema to v2

--- Assets/ClickDungeon/Save/SaveSchema.cs
+++ Assets/ClickDungeon/Save/SaveSchema.cs
@@ -3,7 +3,7 @@ namespace ClickDungeon.Save
     public static class SaveSchema
     {
         public const int ProfileVersion = 1;
-        public const int RunVersion = 1;
+        public const int RunVersion = 2;
         public const int SettingsVersion = 1;
     }
 }
